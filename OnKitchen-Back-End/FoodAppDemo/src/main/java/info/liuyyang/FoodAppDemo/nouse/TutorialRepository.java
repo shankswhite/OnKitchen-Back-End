4f7bb09c481ae616e302ce2f132476e96b5bf970
Xiaofeng Zhao <zxf6911518@qq.com>
2024-04-27 11:00:42 +0800

    Delete OnKitchen-Back-End directory

--- OnKitchen-Back-End/FoodAppDemo/src/main/java/info/liuyyang/FoodAppDemo/nouse/TutorialRepository.java
+++
@@ -1,8 +0,0 @@
-package info.liuyyang.FoodAppDemo.nouse;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TutorialRepository extends MongoRepository<Tutorial, ObjectId> {}
